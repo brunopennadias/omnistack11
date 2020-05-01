b21a4a0c78a0c920c0e61a920dc3a5d3fc0dcb8e
brunopennadias <brunopennadias@gmail.com>
2020-05-01 08:42:43 -0300

    mobile project

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -17,6 +17,23 @@ export default StyleSheet.create({
   headerText: {
     fontSize: 15,
     color: '#737380',
-    
+  },
+
+  headerTextBold: {
+    fontWeight: 'bold'
+  },
+
+  title: {
+    fontSize: 30,
+    marginBottom: 16,
+    marginTop: 48,
+    color: '#13131a',
+    fontWeight: 'bold'
+  },
+
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#737380'
   }
 });
